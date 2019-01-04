8e232ace3d32118e7e1730be55f4426bf7aff30d
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-04 03:22:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -51,10 +51,10 @@ end
 
 def runner
   welcome
-  card_total = initial_round
-  until card_total > 21 do
-    hit?(card_total)
-    display_card_total(card_total)
+  hand += initial_round
+  until hand > 21 do
+    hand += hit?
+    display_card_total(hand)
   end
   end_game(card_total)
 end
