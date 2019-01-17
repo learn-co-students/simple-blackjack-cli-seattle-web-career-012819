7ae1f6ada0a18bc41bea6c297e90a7b505188e72
github username <github email address>
2019-01-16 19:51:53 -0800

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,8 @@ require_relative "../lib/blackjack.rb"
 
 RSpec.configure do |config|
 # config here
+
+  expect($stdout).to receive(:puts).with("Type 'h' to hit or 's' to stay")
+
+  expect(self).to receive(:get_user_input).and_return("s")
 end
